a094c64d5ac08a7b3aa9d2e34e4fbb2a30e743e9
M. Yousaf Ramzan <yousaf0811@gmail.com>
2023-01-07 18:09:22 +0000

    2nd commit router dom add

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-
+import { Routes, Route, BrowserRouter } from "react-router-dom"
+import Home from "./components/home";
 import './App.css';
-
 function App() {
   return (
     <div className="App">
-      <h1>Helo </h1>
+      <BrowserRouter>
+      <Routes>
+      <h1>hdfelo</h1>
+        <Route path="/" element={<Home/>}/>
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
-
 export default App;
