ba4133dbb2cf8eff71056f0472b35cff529d35c0
M. Yousaf Ramzan <yousaf0811@gmail.com>
2023-01-08 07:46:58 +0000

    with node

--- src/components/signupcard.js
+++ src/components/signupcard.js
@@ -1,6 +1,47 @@
 import { useNavigate } from "react-router-dom"
+import { useState } from "react"
 const SignupCard = ()=>{
+  
+  const [userName, setUserName] = useState(null);
+  const [email, setEmail] = useState(null);
+  const [password, setPassword] = useState(null);
   const navigate = useNavigate();
+  const handleInputChange = (e) => {
+    const { id, value } = e.target;
+    if (id === "userName") {
+      setUserName(value);
+    }
+    if(id === "email"){
+        setEmail(value);
+    }
+    if(id === "password"){
+        setPassword(value);
+    }
+  };
+  const handleSubmit = async () => {
+    // event.preventDefault();
+  // console.log(name);
+  if(!userName ) { alert('Fill the empty input fields');  return }; 
+  const user = {
+      userName,
+      email,
+      password
+  }
+  const response = await fetch("http://localhost:8080/create", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json", 
+    },
+    body: JSON.stringify(user),
+  })
+  const result = await response.json()
+  console.log(result)
+  if(response.ok){
+      navigate('/login')
+  }else{
+      navigate('/')
+  }
+};
     return(
         <div>
             <section class="vh-100 gradient-custom">
@@ -13,15 +54,15 @@ const SignupCard = ()=>{
               <h2 class="fw-bold mb-2 text-uppercase">Sign UP</h2>
               <p class="text-white-50 mb-5">Please Enter your Information</p>
               <div class="form-outline form-white mb-3">
-                <input placeholder="Username" type="name" id="typeEmailX" class="form-control form-control-lg" />
+                <input onChange={(e) => handleInputChange(e)} placeholder="Username" type="name" id="userName" class="form-control form-control-lg" />
               </div>
               <div class="form-outline form-white mb-3">
-                <input placeholder="Email" type="email" id="typeEmailX" class="form-control form-control-lg" />
+                <input onChange={(e) => handleInputChange(e)} placeholder="Email" type="email" id="email" class="form-control form-control-lg" />
               </div>
               <div class="form-outline form-white mb-3">
-                <input placeholder="Password" type="password" id="typePasswordX" class="form-control form-control-lg" />
+                <input onChange={(e) => handleInputChange(e)} placeholder="Password" type="password" id="password" class="form-control form-control-lg" />
               </div>
-              <button onClick={() => { navigate('/login') }} class="btn btn-outline-light btn-lg px-5" type="submit">Sign UP</button>
+              <button onClick={() => handleSubmit()} class="btn btn-outline-light btn-lg px-5" type="submit">Sign UP</button>
             </div>
             <div>
               <p class="mb-0">Already Have Account <a href="#!" onClick={() => { navigate('/login') }} class="text-white-50 fw-bold">Log In</a>
